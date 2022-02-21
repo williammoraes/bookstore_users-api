abcc3496d918382a8764ebbeeb7f2065c9b2ba40
William Moraes <williammoraes@gmail.com>
2022-02-20 21:19:15 -0300

    create rest error handlers

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/williammoraes/bookstore_users-api/domain/users"
+	"github.com/williammoraes/bookstore_users-api/services"
+	"github.com/williammoraes/bookstore_users-api/utils/errors"
 )
 
 func GetUser(c *gin.Context) {
@@ -11,7 +14,21 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadResquesError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+
+	if saveErr != nil {
+		c.JSON(http.StatusUnprocessableEntity, saveErr)
+		return
+	} else {
+		c.JSON(http.StatusCreated, result)
+	}
 }
 
 func SearchUser(c *gin.Context) {
